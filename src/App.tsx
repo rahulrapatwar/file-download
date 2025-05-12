ff0bb729acf271bd763ad25c0625d8504a5fe2c6
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2025-05-11 17:28:51 -0700

    add FileTable component and pass data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { FileTable } from "./components/FileTable";
+import { data } from "./data/file";
 
 const App: React.FC = () => {
-  return <div className="p-8 text-[#3B3939]">Test</div>;
+  return (
+    <div className="p-8 text-[#3B3939]">
+      <FileTable data={data} />
+    </div>
+  );
 };
 
 export default App;
