7d886f3cfa537613c90932a6027a0bd93e246207
Rahul Rapatwar <rahulrapatwar@yahoo.com>
2025-05-11 17:59:36 -0700

    create checkbox and download button

--- src/components/FileTable.tsx
+++ src/components/FileTable.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import type { FileData } from "../types";
+import DownloadButton from "./DownloadButton";
 
 interface FileTableProps {
   data: FileData[];
 }
 
 export const FileTable: React.FC<FileTableProps> = ({ data }) => {
+
+  const handleDownload = () => {
+    //TODO
+  };
   return (
-    <div>
-      {data.map((item) => (
-        <div>{item.path}</div>
-      ))}
+    <div className="p-3">
+      <div className="flex items-center m-3 text-lg">
+        <span className="w-4">
+          <input type="checkbox" className="w-4 h-4" />
+        </span>
+        <span className="w-38 p-3">Selected</span>
+        <span>
+          <DownloadButton onClick={handleDownload} isDisabled={false} />
+        </span>
+      </div>
     </div>
   );
 };
